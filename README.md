a88322bad4144a8b8f3bf5d18e651dbde4d56ad3
Aiden <70984367+Nanobot567@users.noreply.github.com>
2022-10-18 19:01:28 +0000

    loom 2

--- README.md
+++ README.md
@@ -2,19 +2,18 @@
 ![version](https://img.shields.io/github/v/release/Nanobot567/loom?label=version)
 ![size](https://img.shields.io/github/repo-size/Nanobot567/loom?label=size)
 
-The encryptor/decryptor
+The encryptor and decryptor.
 
 ## So... what is it?
 Loom is a file encryptor. Simple as that.
 
-## How does it encrypt files?
-Essentially, Loom searches through the file that you point it to. It gets the Unicode code point for each character, and converts it to a hex code on the fly. As soon as it's converted, an integer chosen by the user (called the loomcode) is converted to a hex code as well and is added to that same character. This process is repeated until the file ends, and as soon as it's done, Loom spits out a new file. 
+## How does it encrypt files, and how easy is it to crack the loomcode?
+Essentially, Loom searches through the file that you point it to. It gets the Unicode code point for each character, and converts it to a hex code on the fly. As soon as it's converted, an integer chosen by the user (called the loomcode) is converted to a hex code as well and is added to that same character. This process is repeated until the file ends, and as soon as it's done, Loom spits out a new file.
 
-## What kind of encryption is that?! You can crack it super easily!
-Well, if the user chooses a REALLY simple loomcode for their encryption, then yeah, it's really easy to find the correct one. The thing is, though, there are thousands of numbers you can use as your loomcode. Sure, your hex values will be huge, but it would be a LOT harder to get the correct loomcode.
+As for ease of cracking the encryption, it really depends on the loomcode the user chooses. If the user chooses a REALLY simple loomcode for their encryption, then it's really easy to find the correct one. The thing is, though, there are thousands of numbers you can use as your loomcode. Sure, your hex values will be huge, but it would be a LOT harder to get the correct loomcode.
 
 ## Limitations?
-Python hates hex codes. You can't go over 0x110000, you can't write some chr() values. It STINKS! I would do C++ or something else, but Python is the only language I'm really fluent in. So, as of now, brute-forcing is kind of a bust :(
+You can only write characters that are in Unicode, meaning you cannot write characters that have a hex value higher than 0x110000.
 
 ## Why should I care?
 You shouldn't. I just made this for fun.
--- loom.py
+++ loom.py
@@ -1,36 +1,33 @@
-from os import read
-
 yn = ""
 
 try:
     from alive_progress import alive_bar
-except:
+except Exception:
     print("\nWarning: alive_progress not installed! Would you like to install it using pip? [y/n]")
     while True:
         yn = input("> ")
         if yn == "y":
             try:
                 from pip import _internal
-            except:
+            except Exception:
                 print("ERROR: pip is not installed!")
                 quit()
-            try:
-                _internal.main(["install","alive-progress"])
-                print("Please restart loom.")
-                quit()
-            except:
-                print("ERROR: _internal not supported!")
-                quit()
+
+            _internal.main(["install","alive-progress"])
+            print("Please restart loom.")
+            quit()
         elif yn == "n":
             quit()
 
 
 inputstr = ""
 loomcode = ""
+usefile = ""
+mode = ""
 final = ""
 ans = ""
 debug = False
-ver = "1.0"
+ver = "2.0"
 
 
 print(f"""
@@ -43,140 +40,127 @@ print(f"""
   ██  ██    ██  ██    ██  ██   ██   ██
   ██   ██████    ██████   ██   ██   ██
 
-        encryption/decryption
-                 {ver}
+         encryption/compression
+                  {ver}
 
 
 
 """)
 while ans != "q":
-    print("Encrypt, decrypt, help, or quit? [e/d/h/q]")
-    ans = input("> ")
+    ans = input("Encryption, help, or quit? [e/h/q] ")
 
     final = ""
     if ans == "e":
-        print("Would you like to use a file as input? [y/n]")
-        usefile = input("> ")
-        if usefile == "y":
+        while mode.lower() not in ["e","d"]:
+            mode = input("Encrypt or decrypt? [e/d] ")
+        while usefile.lower() not in ["y","n"]:
+            usefile = input("Would you like to use a file as input? [y/n] ")
+        
+        if usefile.lower() == "y":
             filething = input("Where is the file you would like to encrypt? ")
-            rfile = open(filething,"r")
+            rfile = open(filething,"r", encoding="utf-8")
             fileout = input("What should the output file be called? ")
         else:
-            inputstr = input("enter text to encode: ")
+            inputstr = input("Enter text: ")
         loomcode = input("loomcode (integer): ")
         # loomcode = hex(int(loomcode))
 
-        print("Encrypting...")
+        print("Please wait...")
         char = 0
-        if usefile == "y":
-            readstuff = rfile.read()
-            if debug:
-                print(readstuff)
-            try:
-                with alive_bar(len(readstuff)) as bar:
-                    for i in readstuff:
-                        char += 1
-                        # ri = hex(int(ord(i)))
-                        ri = hex(int(ord(i))+int(loomcode))
-                        if char == (len(readstuff)):
-                            final += str(ri)
-                        else:
-                            final += str(ri)+","
-                        if debug:
-                            print(ri)
-                        bar()
-            except TypeError as err:
-                print(f"\nERROR! ({err})")
-        else:
-            try:
-                with alive_bar(len(inputstr)) as bar:
+        if usefile.lower() == "y":
+            inputstr = rfile.read()
+
+        if debug:
+            print(inputstr)
+
+        try:
+            if mode == "e":
+                with alive_bar(len(inputstr), bar="classic", spinner="classic") as bar:
                     for i in inputstr:
                         char += 1
                         # ri = hex(int(ord(i)))
-                        ri = hex(int(ord(i))+int(loomcode))
-                        if char == len(inputstr):
-                            final += str(ri)
-                        else:
-                            final += str(ri)+","
+                        ri = int(ord(i))+int(loomcode)
+                        final += chr(ri)
                         if debug:
                             print(ri)
+                        bar.text(i+" encrypted")
                         bar()
-            except TypeError as err:
-                print(f"ERROR! ({err})")
-        if debug:
-            print(final)
-        if usefile == "y":
-            try:
-                f = open(fileout+".loomed","w")
-                f.write(final)
-                f.close()
-            except UnicodeEncodeError as err:
-                print(f"\nERROR! ({err})")
-        else:
-            try:
-                f = open("loom_out.loomed","w")
-                f.write(final)
-                f.close()
-            except UnicodeEncodeError as err:
-                print(f"\nERROR! ({err})")
+            else:
+                with alive_bar(len(inputstr), bar="classic", spinner="classic") as bar:
+                    for i in inputstr:
+                        # ir = chr(int(i, 0))
+                        ir = chr(int(ord(i))-int(loomcode))
+                        final += str(ir)
+                        bar.text(i+" decrypted")
+                        bar()
 
-        print("Finished!")
-    elif ans == "d":
-        print("Would you like to use a file as input? [y/n]")
-        usefile = input("> ")
-        if usefile == "y":
-            filething = input("Where is the file you would like to decrypt? ")
-            rfile = open(filething,"r")
-            fileout = input("What should the output file be called? ")
-        else:
-            inputstr = input("Enter hex to decode (each hex code separated by a comma): ")
-
-        loomcode = input("loomcode (integer): ")
-
-        print("Decrypting...")
-
-        if usefile == "y":
-            istr = rfile.read().split(",")
-        else:
-            istr = inputstr.split(",")
-
-        if debug:
-            print(istr)
-
-        try:
-            with alive_bar(len(istr)) as bar:
-                for i in istr:
-                    # ir = chr(int(i, 0))
-                    ir = chr(int(i, 0)-int(loomcode))
-                    final += str(ir)
-                    bar()
+        except TypeError as err:
+            print(f"\nERROR! ({err})")
         except Exception as err:
-            print(f"ERROR! ({err})")
-        
+            print(f"\nERROR! ({err})")
+
         if debug:
             print(final)
-        
-        try:
-            f = open(fileout,"w")
-            f.write(final)
-            f.close()
-        except UnicodeEncodeError as err:
-            print(f"ERROR! ({err})")
+
+        if usefile == "y":
+            try:
+                f = open(fileout,"w", encoding="utf-8")
+                f.write(final)
+                f.close()
+            except UnicodeEncodeError as err:
+                print(f"\nERROR! ({err})")
 
         print("Finished!")
+    # elif ans == "c":
+    #     mode = input("Compress or decompress? [c/d] ")
+    #     usefile = input("Would you like to use a file as input? [y/n] ")
+    #     if usefile == "y":
+    #         filething = input("Where is the file? ")
+    #         rfile = open(filething,"r")
+    #         fileout = input("What should the output file be called? ")
+    #         inputstr = rfile.read()
+    #     else:
+    #         inputstr = input("Enter text to encode: ")
+
+    #     origFileLen = len(inputstr)
+
+    #     if mode == "c":
+    #         with alive_bar(len(inputstr)) as bar:
+    #             for i in range(int(len(inputstr))):
+    #                 # ir = chr(int(i, 0))
+    #                 final += compTable[hex(ord(inputstr[i])).split("0x")[1]]
+    #                 bar()
+            
+    #         print(origFileLen / len(final))
+
+    #     else:
+    #         with alive_bar(len(inputstr)) as bar:
+    #             for i in inputstr:
+    #                 # ir = chr(int(i, 0))
+    #                 final += chr(int(list(compTable.keys())[list(compTable.values()).index(i)], 16))
+    #                 bar()
+            
+    #     if usefile == "y":
+    #         try:
+    #             f = open(fileout,"w")
+    #             f.write(final)
+    #             f.close()
+    #         except UnicodeEncodeError as err:
+    #             print(f"\nERROR! ({err})")
+    #     else:
+    #         print(final)
+        
 
     elif ans == "h":
         print("\n\nWelcome to Loom!")
         print("\n----What is Loom?----\n")
-        print("Loom is a file encryptor. It uses an encryption method where hex codes are replaced with the characters in the file. Read the README.md for info on how it works.")
+        print("Loom is a file encryptor and compressor. It uses an encryption method where hex codes are replaced with the characters in the file. Read the README.md for info on how it works.")
         print("\n----Commands----\n")
-        print("There are 4 commands available:\n")
-        print(" e (encrypt) - encrypt files or plaintext")
-        print(" d (decrypt) - decrypt your already encrypted files or plaintext")
+        print("There are 3 commands available:\n")
+        print(" e (encryption) - encrypt or decrypt files or plaintext")
+        # print(" c (compression) - compresses or decompresses files or plaintext")
         print(" h (help) - tells about how to use loom")
         print(" q (quit) - get outta here!\n")
-        
+      
     elif ans == "q":
         break
-
-
